0a902813215425af6b6d8115ad3ac0cd984a4812
jangjs1216 <ds10079@naver.com>
2022-02-23 17:01:51 +0900

    ㅁㅁ

--- app/src/main/java/com/example/ssuk/CarGame.java
+++ app/src/main/java/com/example/ssuk/CarGame.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.opengl.Visibility;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
+
+import java.util.Random;
 
 public class CarGame extends AppCompatActivity {
 
@@ -32,6 +35,59 @@ public class CarGame extends AppCompatActivity {
         ImageView turn_right_gif = (ImageView) findViewById(R.id.turn_right_gif);
         Glide.with(this).load(R.drawable.turn_right_remove).into(turn_right_gif);
 
+        jump_gif.setVisibility((View.INVISIBLE));
+        start_gif.setVisibility((View.INVISIBLE));
+        stop_gif.setVisibility((View.INVISIBLE));
+        turn_left_gif.setVisibility((View.INVISIBLE));
+        turn_right_gif.setVisibility((View.VISIBLE));
 
+        jump_gif.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Random random = new Random();
+                int i = random.nextInt(5)+1;
+                if(i == 1)
+                {
+                    jump_gif.setVisibility((View.VISIBLE));
+                    start_gif.setVisibility((View.INVISIBLE));
+                    stop_gif.setVisibility((View.INVISIBLE));
+                    turn_left_gif.setVisibility((View.INVISIBLE));
+                    turn_right_gif.setVisibility((View.INVISIBLE));
+                }
+                if(i == 2)
+                {
+                    jump_gif.setVisibility((View.INVISIBLE));
+                    start_gif.setVisibility((View.VISIBLE));
+                    stop_gif.setVisibility((View.INVISIBLE));
+                    turn_left_gif.setVisibility((View.INVISIBLE));
+                    turn_right_gif.setVisibility((View.INVISIBLE));
+                }
+                if(i == 3)
+                {
+                    jump_gif.setVisibility((View.INVISIBLE));
+                    start_gif.setVisibility((View.INVISIBLE));
+                    stop_gif.setVisibility((View.VISIBLE));
+                    turn_left_gif.setVisibility((View.INVISIBLE));
+                    turn_right_gif.setVisibility((View.INVISIBLE));
+                }
+                if(i == 4)
+                {
+                    jump_gif.setVisibility((View.INVISIBLE));
+                    start_gif.setVisibility((View.INVISIBLE));
+                    stop_gif.setVisibility((View.INVISIBLE));
+                    turn_left_gif.setVisibility((View.VISIBLE));
+                    turn_right_gif.setVisibility((View.INVISIBLE));
+                }
+                if(i == 5)
+                {
+                    jump_gif.setVisibility((View.INVISIBLE));
+                    start_gif.setVisibility((View.INVISIBLE));
+                    stop_gif.setVisibility((View.INVISIBLE));
+                    turn_left_gif.setVisibility((View.INVISIBLE));
+                    turn_right_gif.setVisibility((View.VISIBLE));
+                }
+
+            }
+        });
     }
 }
