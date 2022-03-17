b5a94e4d5ca6ef9d20d5cea08f0001d290c7ea37
DjkimYO <kdream2620@naver.com>
2022-03-17 17:47:32 +0900

    fix

---
+++ app/src/main/java/com/example/ssuk/StartActivity.java
@@ -0,0 +1,29 @@
+package com.example.ssuk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        LinearLayout screen = (LinearLayout) findViewById(R.id.screen);
+
+        screen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(StartActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
