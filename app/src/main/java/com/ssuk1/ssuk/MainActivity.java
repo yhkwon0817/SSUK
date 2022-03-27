9bf495664f88731f70df588d83a3cdb3f90f0d3d
DjkimYO <kdream2620@naver.com>
2022-03-27 18:02:53 +0900

    math to phone or tablet

--- app/src/main/java/com/ssuk1/ssuk/MainActivity.java
+++ app/src/main/java/com/ssuk1/ssuk/MainActivity.java
@@ -3,8 +3,10 @@ package com.ssuk1.ssuk;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Point;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.Display;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
@@ -28,6 +30,19 @@ public class MainActivity extends AppCompatActivity {
         ImageView logo2 = (ImageView) findViewById(R.id.picturegame_title);
         ImageView logo3 = (ImageView) findViewById(R.id.cargame_title);
         ImageView logo4 = (ImageView) findViewById(R.id.liargame_title);
+        ImageView imageView = (ImageView)findViewById(R.id.img_bottom);
+
+        Display display = getWindowManager().getDefaultDisplay();  // in Activity
+        /* getActivity().getWindowManager().getDefaultDisplay() */ // in Fragment
+        Point size = new Point();
+        display.getRealSize(size); // or getSize(size)
+        int width = size.x;
+        int height = size.y;
+
+        if((width==1440&&height==2560)||(width==1080&&height==1920))
+            imageView.setVisibility(View.GONE);
+        if(height>=2000)
+            imageView.setScaleType(ImageView.ScaleType.FIT_XY);
 
         logo1.setOnClickListener(new View.OnClickListener() {
             @Override
